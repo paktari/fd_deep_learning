8d888456c4b3b5294fb2ea62e467e5819c30909e
paktari <45333798+paktari@users.noreply.github.com>
2024-09-12 14:26:09 +0700

    Add files via upload

--- pages/Data Cleansing.py
+++ pages/Data Cleansing.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
-
 import pandas as pd
+#from sklearn.ensemble import RandomForestClassifier
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
