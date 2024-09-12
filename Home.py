c3dac51bef37d96eb254594b9263eb3a0e012b11
paktari <45333798+paktari@users.noreply.github.com>
2024-09-12 08:56:35 +0700

    Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,23 @@ st.set_page_config(
     page_title="Home",
     page_icon="🏠",
 )
+st.markdown("<h1 style='text-align: center; color: red;'>Prediksi Financial Distress UMKM</h1>",
+                unsafe_allow_html=True)
+    st.write('\n')
+    # User Information
+    st.sidebar.markdown("""
+    Data yang perlu diisi : 
+                
+        1. Nama UMKM
+        2. Penjualan
+        3. Laba Kotor
+        4. Laba Bersih
+        5. Aset Lancar
+        6. Aset Tetap
+        7. Hutang Jangka Pendek
+        8. Hutang Jangka Panjang
+        9. Modal Sendiri
+        """)
 
 st.header("Selamat Datang! 👋")
 st.write('Financial Distress deep learning')
