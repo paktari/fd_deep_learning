5f84910843f8c960572312f36da826072fc2af17
paktari <45333798+paktari@users.noreply.github.com>
2024-09-12 13:41:32 +0700

    Update 1_Deskripsi.py

--- pages/1_Deskripsi.py
+++ pages/1_Deskripsi.py
@@ -6,11 +6,11 @@ st.set_page_config(page_title="Deskripsi", page_icon="1️⃣")
 st.header("Penjelasan Fiancial Distress")
 
 st.markdown('''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
+Financial distress adalah penurunan kinerja keuangan perusahaan.
+Setiap perusahaan pasti memiliki fase naik dan turun performa, termasuk dari segi keuangan. Financial distress adalah istilah untuk menyebut kondisi penurunan performa tersebut. Apabila tidak segera ditangani sumbernya, financial distress akan berujung pada kebangkrutan. 
+Financial distress adalah peristiwa penurunan kinerja keuangan perusahaan secara terus menerus dalam jangka waktu tertentu. Bagi perusahaan, financial distress adalah salah satu kondisi penyebab kebangkrutan paling sering. Sebab berbeda dengan penurunan laba biasa, nominal kerugian karena financial distress bisa sangat besar hingga mempengaruhi kelancaran operasional perusahaan.
 
-Fusce ultrices tincidunt elit imperdiet dapibus. Nunc condimentum fermentum lacinia. Duis vestibulum neque posuere lorem tempus iaculis. Nulla ultrices accumsan est quis ullamcorper. Morbi convallis congue dui eu congue. Quisque laoreet turpis commodo luctus varius. Aliquam et iaculis erat. Mauris bibendum sem leo, id ultricies sapien viverra sed. Nunc id erat quis justo sagittis viverra ut id erat.
 
-Duis ultricies, sem eu fermentum fermentum, nisl tellus egestas odio, at placerat augue quam eget justo. Nullam facilisis porttitor ante, vitae blandit odio dapibus at. Nam tellus arcu, tristique sit amet posuere ut, posuere id nibh. Fusce ut diam dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc metus sapien, feugiat sed diam in, facilisis sollicitudin eros. Quisque quis ex et nunc pellentesque vestibulum. Praesent interdum molestie orci, id faucibus lorem blandit consequat. Vivamus aliquam, erat eu facilisis lacinia, felis nunc tristique lectus, iaculis pretium lectus massa eget risus. Pellentesque rhoncus arcu vel elementum venenatis. Ut in blandit libero, eget facilisis magna. Morbi aliquam elit ut neque imperdiet scelerisque.
 ''')
 
 load_css()
