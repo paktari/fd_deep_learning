6c13f5839baf891bc4d93697a1f1d540b61b5579
paktari <45333798+paktari@users.noreply.github.com>
2024-10-17 11:33:54 +0700

    Add files via upload

--- Home.py
+++ Home.py
@@ -5,10 +5,40 @@ st.set_page_config(
     page_title="Home",
     page_icon="🏠",
 )
-st.markdown("<h1 style='text-align: center; color: red;'>Prediksi Financial Distress UMKM</h1>", unsafe_allow_html=True)
-st.write('\n')
+
+from PIL import Image
+image = Image.open('company.jpg')
+
+st.image(image, caption='~')
+
 
 st.header("Selamat Datang! 👋")
-st.write('Financial Distress deep learning')
+st.write('Financial Distress dengan Deep Learning')
+st.write(
+    """
+    # Definisi
+
+    Prediksi kebangkrutan perusahaan adalah proses menggunakan berbagai metode analisis
+    untuk mengevaluasi kesehatan keuangan suatu perusahaan dan memprediksi apakah
+    perusahaan tersebut berisiko menghadapi kebangkrutan di masa depan.
+
+    Tujuan dari prediksi kebangkrutan perusahaan adalah memberikan informasi kepada
+    pemangku kepentingan, seperti pemilik saham, kreditor, dan pemasok, agar mereka dapat
+    mengambil langkah-langkah yang tepat untuk mengurangi risiko atau melindungi
+    kepentingan mereka.
+
+    Variabel yang akan kami gunakan pada penelitian ini adalah
+    1. Penjualan
+    2. Laba Kotor
+    3. Laba Bersih
+    4. Aset Lancar
+    5. Aset Tetap
+    6. Hutang Jangka Pendek
+    7. Hutang Jangka Panjang
+    8. Modal Sendiri
+
+    Metode Deep Learning digunakan untuk memprediksi kebangkrutan UKM Batik
+    """
+)
 
 load_css()
